8605d98a27fbd9bb097ff57b24d9767c96ce9e84
Aalu_chipas <119433529+avinash893@users.noreply.github.com>
2026-05-18 11:31:41 +0530

    Initial commit

---
+++ services/auth/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config({ override: true });
+
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`auth service is running on port ${PORT}`);
+  connectDB();
+});
+
+// Trigger restart
