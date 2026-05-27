c64391e6f1c48bd663928db38937ab7f858c7ef9
Aalu_chipas <119433529+avinash893@users.noreply.github.com>
2026-05-27 14:30:37 +0530

    Implemented Google auth setup

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,5 +1,75 @@
-import { Request, Response } from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import TryCatch from "../middlewares/trycatch.js";
+import { AuthenticatedRequest } from "../middlewares/isAuth.js";
+import { oauth2client, Oauth2Client } from "../config/googleConfig.js";
 
-export const loginUser = async (req: Request, res: Response) => {
-  res.json(req.body);
-};
+export const loginUser = TryCatch(async (req, res) => {
+  const { code } = req.body;
+  if (!code) {
+    return res.status(400).json({ message: "Authorization code is required" });
+  }
+
+  const googleResponse = await oauth2client.getToken(code);
+
+  oauth2client.setCredentials(googleResponse.tokens);
+
+  const { email, name, picture } = req.body;
+
+  let user = await User.findOne({ email });
+
+  if (!user) {
+    user = await User.create({
+      name,
+      email,
+      image: picture,
+    });
+  }
+
+  const token = jwt.sign({ user: user }, process.env.JWT_SECRET!, {
+    expiresIn: "1h",
+  });
+
+  res.status(200).json({
+    message: "Login successful",
+    token,
+    user,
+  });
+});
+
+const allowedRoles = ["customer", "rider", "seller"] as const;
+
+type Role = (typeof allowedRoles)[number];
+
+export const addUserRole = TryCatch(async (req: AuthenticatedRequest, res) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+  const { role } = req.body as { role: Role };
+
+  if (!allowedRoles.includes(role)) {
+    return res.status(400).json({ message: "Invalid role" });
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    { role },
+    { new: true },
+  );
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const token = jwt.sign({ user }, process.env.JWT_SECRET as string, {
+    expiresIn: "1h",
+  });
+
+  res.json({ user, token });
+});
+
+export const myProfile = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const user = req.user;
+  res.json(user);
+});
--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { loginUser } from "../controllers/auth.js";
+import { loginUser, myProfile } from "../controllers/auth.js";
+import { addUserRole } from "../controllers/auth.js";
+import { isAuth } from "../middlewares/isAuth.js";
 
 const router = Router();
 
 router.post("/login", loginUser);
+router.put("/add/role", isAuth, addUserRole);
+router.get("/me", isAuth, myProfile);
 
 export default router;
